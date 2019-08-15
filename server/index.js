70180c25a0b8925687e85ebabff07b021b6e9af1
halojasa <halojasa@halojasas-iMac.local>
2019-08-15 16:54:43 +0700

    creating a server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io");
+const io = require("socket.io").listen(server);
+const port = 3000;
+
+
+io.on("connection", socket => {
+    console.log("User Senang");
+})
+
+server.listen(port, () => console.log("server running on port" + port));
