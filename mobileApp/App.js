69c83bfbc0abad5ddf9d1c8fbbd3591ea4c41b4b
halojasa <halojasa@halojasas-iMac.local>
2019-08-15 17:19:37 +0700

    remove yellow warning

--- mobileApp/App.js
+++ mobileApp/App.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, TextInput, StyleSheet } from 'react-native'
 import io from 'socket.io-client'
+console.disableYellowBox = true
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatMsg: ''
+
+    };
+  }
   componentDidMount() {
     const socket = io("http://192.168.5.42:3000")
   }
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={styles.viewContainer}>
+        <TextInput
+          style={styles.textChat}
+          autoCorrect={false}
+          onChangeText={chatMsg => this.setState({
+            chatMsg
+          })}
+        />
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  viewContainer: {
+    flex: 1
+  },
+  textChat: {
+    height: 40,
+    borderWidth: 1
+  }
+});
+
