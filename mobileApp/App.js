65514678dc8bda603c1058786c6ba6ddbfbab44f
halojasa <halojasa@halojasas-iMac.local>
2019-08-15 17:01:14 +0700

    modified mobile app to adding socket.io-client

--- mobileApp/App.js
+++ mobileApp/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
-
+import io from 'socket.io-client'
 export default class App extends Component {
   render() {
     return (
